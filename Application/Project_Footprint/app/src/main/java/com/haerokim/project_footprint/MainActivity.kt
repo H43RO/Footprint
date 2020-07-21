4663e780b27218a0bd7094026013f9f22b60290d
H43RO <h2is1234@naver.com>
2020-07-21 15:10:11 +0900

    Beacon Scanning Test

--- Application/Project_Footprint/app/src/main/java/com/haerokim/project_footprint/MainActivity.kt
+++ Application/Project_Footprint/app/src/main/java/com/haerokim/project_footprint/MainActivity.kt
@@ -1,11 +1,75 @@
 package com.haerokim.project_footprint
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+import android.os.RemoteException
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import org.altbeacon.beacon.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), BeaconConsumer {
+    private lateinit var beaconManager : BeaconManager
+    private var beaconList: MutableList<Beacon> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        beaconManager =  BeaconManager.getInstanceForApplication(this)
+
+        beaconManager.getBeaconParsers()
+            .add(BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"))
+
+        beaconManager.bind(this)
+
+        var handler: Handler = object : Handler() {
+            override fun handleMessage(msg: Message?) {
+                Textview.setText("")
+
+                // 비콘의 아이디와 거리를 측정하여 textView에 넣는다.
+                for (beacon in beaconList) {
+                    Textview.append(
+                        "ID : " + beacon.id2 + " / " + "Distance : " + String.format(
+                            "%.3f",
+                            beacon.distance
+                        ).toDouble() + "m\n"
+                    )
+                }
+
+                // 자기 자신을 0.5초마다 호출
+                this.sendEmptyMessageDelayed(0, 500)
+            }
+        }
+
+        button.setOnClickListener {
+            handler.sendEmptyMessage(0)
+            Toast.makeText(this,"시작",Toast.LENGTH_LONG).show()
+        }
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        beaconManager.unbind(this)
+    }
+
+    override fun onBeaconServiceConnect() {
+        beaconManager.setRangeNotifier(RangeNotifier { beacons, region ->
+            // 비콘이 감지되면 해당 함수가 호출된다. Collection<Beacon> beacons에는 감지된 비콘의 리스트가,
+            // region에는 비콘들에 대응하는 Region 객체가 들어온다.
+            if (beacons.size > 0) {
+                beaconList.clear()
+                for (beacon in beacons) {
+                    beaconList.add(beacon)
+                }
+            }
+        })
+
+        try {
+            beaconManager.startRangingBeaconsInRegion(Region("myRangingUniqueId", null, null, null))
+        } catch (e: RemoteException) {
+        }
+    }
+
 }
