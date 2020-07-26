76303696430687f6ac28f7da1159271c43718bf9
DESKTOP-2PAA9OC\예원 <noddy0501@gmail.com>
2020-07-27 01:00:30 +0900

    [yewon] place model 구현
    - setting.py : INSTALLED_APPS에 website 폴더 추가했습니다.
    - urls.py : website url을 추가했습니다.
    - models.py : place-model 구현 작업했습니다.

--- Web/Project_Footprint/website/models.py
+++ Web/Project_Footprint/website/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here..
+
+class Place(models.Model):
+    beacon_uuid = models.CharField(max_length=100)
+    title = models.CharField(max_length=20)
+    place_div = models.PositiveSmallIntegerField()
+    naver_place_id = models.CharField(max_length=100)
