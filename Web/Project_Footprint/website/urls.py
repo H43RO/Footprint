366bd8c97b3b3936443984e8fc5647677922d20a
DESKTOP-2PAA9OC\예원 <noddy0501@gmail.com>
2020-07-27 17:17:19 +0900

    Revert "[yewon] Place-list Form 구현했습니다."
    This reverts commit 31d4e4f425bef7dee272a01d746c25a74b649234.

--- Web/Project_Footprint/website/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('place_list/',views.place_list, name="place_list")
-]
--- Web/Project_Footprint/website/views.py
+++ Web/Project_Footprint/website/views.py
@@ -1,16 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Place
 
 # Create your views here.
 
 def index(request):
     return HttpResponse('발자취')
-
-
-def place_list(request):
-    context = {
-        'place': Place.objects.all()
-    }
-    return render(request, 'place_list.html', context)
-
