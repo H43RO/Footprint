679d01d5846ac2e27ddbc03e4381f8dcc1961af9
DESKTOP-2PAA9OC\예원 <noddy0501@gmail.com>
2020-07-27 17:48:38 +0900

    [yewon] Place-list Form 구현했습니다.
    - urls.py : website url을 추가 및 오류 수정 작업 했습니다.
    - views.py : place-list 구현 작업했습니다.
    - place_list.html : place-list를 확인만 하기 위하여 카드 형식으로 구현했습니다.

---
+++ Web/Project_Footprint/website/urls.py
@@ -0,0 +1,7 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('place_list/', views.place_list, name="place_list")
+]
--- Web/Project_Footprint/website/views.py
+++ Web/Project_Footprint/website/views.py
@@ -1,7 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Place
 # Create your views here.
 
 def index(request):
+
     return HttpResponse('발자취')
+
+
+def place_list(request):
+    context = {
+        'places': Place.objects.all()
+    }
+    return render(request, 'place_list.html', context)
