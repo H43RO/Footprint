fc62df94edfacc50e464f982b7914f5f0c8d3559
DESKTOP-2PAA9OC\예원 <noddy0501@gmail.com>
2020-07-27 22:04:00 +0900

    [YulHee] History Model  누락되어서 추가했습니다.
    - website/models.py : history 모델 누락되어서 추가했습니다.

--- Web/Project_Footprint/website/models.py
+++ Web/Project_Footprint/website/models.py
@@ -8,3 +8,19 @@ class Place(models.Model):
     place_div = models.PositiveSmallIntegerField()
     naver_place_id = models.CharField(max_length=30)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class History(models.Model):
+    #user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    img = models.CharField(max_length=600, default=None, null=True)
+    title = models.CharField(max_length=100)
+    comment = models.CharField(max_length=1000)
+
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
