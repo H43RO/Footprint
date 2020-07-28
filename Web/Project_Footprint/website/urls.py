2166a393ad6929e7fbb102ad802626e83fc7ea56
HAERO_KR <h2is1234@naver.com>
2020-07-29 00:03:53 +0900

    Revert "[yewon] Place 구분 구현했습니다."

--- Web/Project_Footprint/website/urls.py
+++ Web/Project_Footprint/website/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('signup/', views.signup, name='signup'),
+    path('list/', views.list, name='list'),
     path('history/', views.history, name='history'),
     path('place_list/', views.place_list, name='place_list'),
-    path('place_register/', views.place_register, name='place_register'),
-    path('index/', views.index, name='index'),
-    path('place_restaurant_list/', views.place_restaurant, name='place_restaurant_list'),
-    path('place_sights_list/', views.place_sights, name='place_sights_list'),
+    path('place_register/', views.place_register, name='place_register')
 ]
+
