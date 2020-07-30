1a84561383a963cb718d50162bf2e60c8d01acd1
김율희 <dbfgml741@likelion.org>
2020-07-31 03:04:38 +0900

    [YulHee]: History 뼈대 기능 구현
    -website/models.py : history model 기능 추가 및 Place 모델 타이틀 보여주는 함수 추가

--- Web/Project_Footprint/website/models.py
+++ Web/Project_Footprint/website/models.py
@@ -73,6 +73,8 @@ class Place(models.Model):
     naver_place_id = models.CharField(max_length=30)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.title
 
 class History(models.Model):
     img = models.ImageField(blank=True, upload_to="blog/%Y/%m/%d")
