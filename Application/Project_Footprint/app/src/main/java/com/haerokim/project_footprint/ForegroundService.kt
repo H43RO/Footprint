d6a52a8af55f2dd36eebb3329d2b6095d557ce4a
yywon <57125912+hyywon@users.noreply.github.com>
2020-08-06 20:09:59 +0900

    Update ForegroundService.kt

--- Application/Project_Footprint/app/src/main/java/com/haerokim/project_footprint/ForegroundService.kt
+++ Application/Project_Footprint/app/src/main/java/com/haerokim/project_footprint/ForegroundService.kt
@@ -74,11 +74,8 @@ class ForegroundService : Service(), BeaconConsumer {
             .add(BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"))
 
         var retrofit = Retrofit.Builder()
-<<<<<<< HEAD
             .baseUrl("http://5e637d81aee0.ngrok.io/") //사이트 Base URL
-=======
             .baseUrl(Website.baseUrl) //사이트 Base URL
->>>>>>> master
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
