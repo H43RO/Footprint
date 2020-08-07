f608fe247b9d87fa607e5f79783d19d618d19d0a
H43RO <h2is1234@naver.com>
2020-08-07 23:56:58 +0900

    [HyeonJun] 프로필 사진 등록 및 변경 기능 추가
    - EditProfileFragment.kt : Android-image-cropper 라이브러리와 SharedPreferences를 활용하여 프로필 사진 등록 및 변경 기능을 구현하였습니다.
    - HomeFragment.kt : 프로필 사진 영역에 등록 및 변경한 프로필 사진이 보이도록 구현하고, 터치 시 메뉴 Fragment로 이동하는 동작을 구현했습니다.
    - MenuFragment.kt :  프로필 사진 영역에 등록 및 변경한 프로필 사진이 보이도록 구현하였습니다.

--- Application/Project_Footprint/app/src/main/java/com/haerokim/project_footprint/Data/Website.kt
+++ Application/Project_Footprint/app/src/main/java/com/haerokim/project_footprint/Data/Website.kt
@@ -2,6 +2,6 @@ package com.haerokim.project_footprint.Data
 
 class Website {
     companion object{
-        const val baseUrl = "http://12c06e10dd62.ngrok.io"
+        const val baseUrl = "http://5caffc8289d3.ngrok.io"
     }
 }
--- Application/Project_Footprint/app/src/main/java/com/haerokim/project_footprint/ui/edit_profile/EditProfileFragment.kt
+++ Application/Project_Footprint/app/src/main/java/com/haerokim/project_footprint/ui/edit_profile/EditProfileFragment.kt
@@ -1,14 +1,21 @@
 package com.haerokim.project_footprint.ui.edit_profile
 
+import android.app.Activity
+import android.app.Activity.RESULT_OK
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.content.SharedPreferences
+import android.graphics.Bitmap
 import android.graphics.drawable.ShapeDrawable
 import android.graphics.drawable.shapes.OvalShape
-import android.os.Build
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import com.google.android.material.snackbar.Snackbar
 import com.google.gson.GsonBuilder
@@ -17,6 +24,8 @@ import com.haerokim.project_footprint.Data.User
 import com.haerokim.project_footprint.Data.Website
 import com.haerokim.project_footprint.Network.RetrofitService
 import com.haerokim.project_footprint.R
+import com.theartofdev.edmodo.cropper.CropImage
+import com.theartofdev.edmodo.cropper.CropImageView
 import io.paperdb.Paper
 import kotlinx.android.synthetic.main.fragment_edit_profile.*
 import retrofit2.Call
@@ -24,29 +33,81 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
 import java.text.SimpleDateFormat
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
-import java.time.format.DateTimeFormatter.ISO_DATE
 import java.util.*
 
 
 class EditProfileFragment : Fragment() {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        //프로필 사진 변경을 위한 사진이 선택 및 편집되면 Uri 형태로 결과가 반환됨
+        if (requestCode === CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            val result = CropImage.getActivityResult(data)
+            if (resultCode === RESULT_OK) {
+                val resultUri = result.uri
+                image_edit_user_profile.setImageURI(resultUri)
+
+                val bitmap =
+                    MediaStore.Images.Media.getBitmap(requireContext().contentResolver, resultUri)
+
+                val uri = bitmapToFile(bitmap!!) // Uri
+                val pref = activity?.getSharedPreferences("profile_image", Activity.MODE_PRIVATE)
+                val editor: SharedPreferences.Editor? = pref?.edit()
+                editor?.putString("profile_image", uri.toString())
+                editor?.commit()
+
+            } else if (resultCode === CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+            }
+        }
+    }
+
+    private fun bitmapToFile(bitmap:Bitmap): Uri {
+        // Get the context wrapper
+        val wrapper = ContextWrapper(context)
+
+        // Initialize a new file instance to save bitmap object
+        var file = wrapper.getDir("Images", Context.MODE_PRIVATE)
+        file = File(file,"profile_image.jpg")
+        try{
+            // Compress the bitmap and save in jpg format
+            val stream: OutputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.JPEG,100,stream)
+            stream.flush()
+            stream.close()
+        }catch (e: IOException){
+            e.printStackTrace()
+            Log.e("Error Saving Image", e.message)
+        }
+        return Uri.parse(file.absolutePath)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_edit_profile, container, false)
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         image_edit_user_profile.setBackground(ShapeDrawable(OvalShape()))
         image_edit_user_profile.setClipToOutline(true)
+
+        val pref: SharedPreferences? = context?.getSharedPreferences("profile_image", Activity.MODE_PRIVATE)
+        val profileImageUri = Uri.parse(pref?.getString("profile_image", ""))
+
+        if(profileImageUri.toString() != ""){
+            image_edit_user_profile.setImageURI(profileImageUri)
+        }else{
+            image_edit_user_profile.setImageResource(R.drawable.basic_profile)
+        }
 
         val user: User = Paper.book().read("user_profile")
 
@@ -60,6 +121,20 @@ class EditProfileFragment : Fragment() {
         edit_profile_birth_date.setText(userBirthDateString)
         edit_profile_age.setText(user.age.toString())
 
+        // 프로필 사진 변경
+        button_change_profile_image.setOnClickListener {
+            CropImage.activity()
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .setActivityTitle("프로필 사진 변경")
+                .setCropShape(CropImageView.CropShape.OVAL)
+                .setCropMenuCropButtonTitle("완료")
+                .setAspectRatio(1, 1)
+                .setRequestedSize(400,400)
+                .start(requireContext(), this)
+
+        }
+
+        // 회원 프로필 정보 수정 취소
         revert_edit_profile.setOnClickListener {
             edit_profile_nickname.setText(user.nickname)
             edit_profile_email.setText(user.email)
@@ -67,6 +142,7 @@ class EditProfileFragment : Fragment() {
             edit_profile_age.setText(user.age.toString())
         }
 
+        // 회원 프로필 정보 수정
         val gson = GsonBuilder()
             .setDateFormat("yyyy-MM-dd")
             .create()
@@ -80,8 +156,7 @@ class EditProfileFragment : Fragment() {
             retrofit.create(RetrofitService::class.java)
 
         submit_edit_profile.setOnClickListener {
-
-            //String to Date
+            // String to Date
             val updateBirthDateString: String = edit_profile_birth_date.text.toString()
             val updateBirthDate: Date = userBirthDateFormat.parse(updateBirthDateString)
 
@@ -119,7 +194,6 @@ class EditProfileFragment : Fragment() {
                         )
 
                         Paper.book().write("user_profile", newUserProfile)
-
                         Snackbar.make(requireActivity().findViewById(android.R.id.content), "회원 정보 변경 완료!", Snackbar.LENGTH_LONG).show()
                     }
                 })
