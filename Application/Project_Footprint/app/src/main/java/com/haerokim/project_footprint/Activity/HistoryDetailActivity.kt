dd9561a879e10d3293df74c1f4a3e86e05292d26
H43RO <h2is1234@naver.com>
2020-08-10 00:07:42 +0900

    [HyeonJun] HistoryDetailActivity 구현
    - HistoryListAdapter.kt : History의 상세보기 페이지로 넘어가는 Intent동작을 onClick 메소드로 등록했습니다. 이 때 History의 정보를 Bundle로 담도록 하였습니다.
    - HistoryDetailActivity.kt : 인텐트로 전달받은 History의 정보를 레이아웃에 뿌려줍니다.

--- Application/Project_Footprint/app/src/main/java/com/haerokim/project_footprint/Activity/HistoryDetailActivity.kt
+++ Application/Project_Footprint/app/src/main/java/com/haerokim/project_footprint/Activity/HistoryDetailActivity.kt
@@ -1,13 +1,45 @@
 package com.haerokim.project_footprint.Activity
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.haerokim.project_footprint.R
+import kotlinx.android.synthetic.main.activity_history_detail.*
+import kotlinx.android.synthetic.main.activity_place_detail.*
+import kotlinx.android.synthetic.main.history_item.*
 
 class HistoryDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history_detail)
+
+        val historyInfo: Bundle? = intent.extras
+
+        var historyID = historyInfo?.getInt("id")
+        var historyImage = historyInfo?.getString("image")
+        var historyTitle = historyInfo?.getString("title") ?: "발자취 : 제목 미상"
+        var historyMood = historyInfo?.getInt("mood") //기본 '0' (감정 - So So)
+        var historyComment = historyInfo?.getString("comment") ?: "발자취 : 내용 미상"
+        var historyPlaceTitle = historyInfo?.getString("placeTitle")
+        var historyCreatedAt = historyInfo?.getString("createdAt") ?: "어느 멋진 날"
+        var historyUserID = historyInfo?.getInt("userID")
+
+        if(historyImage == null){
+            // Image URL 없을 시 기본 이미지
+            history_detail_image.setImageResource(R.drawable.placeholder)
+        }else{
+            Glide.with(this)
+                .load(historyImage)
+                .centerCrop()
+                .thumbnail(0.1f)
+                .into(history_detail_image)
+        }
+
+        text_history_detail_title.text = historyTitle
+        text_history_detail_place.text = historyPlaceTitle
+        text_history_detail_time.text = historyCreatedAt
+        text_history_detail_content.text = historyComment
     }
 }
 
--- Application/Project_Footprint/app/src/main/java/com/haerokim/project_footprint/DataClass/Website.kt
+++ Application/Project_Footprint/app/src/main/java/com/haerokim/project_footprint/DataClass/Website.kt
@@ -2,6 +2,6 @@ package com.haerokim.project_footprint.DataClass
 
 class Website {
     companion object{
-        const val BASE_URL = "http://df36307cc65c.ngrok.io"
+        const val BASE_URL = "http://7306687213ca.ngrok.io"
     }
 }
