4d274930512588316661bb64338d37bd34ebe78f
H43RO <h2is1234@naver.com>
2020-08-16 19:05:45 +0900

    [HyeonJun] HistoryWriteActivity 구현
    - HistoryWriteActivity.kt : 임의 히스토리 작성 Activity 입니다.
    - activity_history_write.xml : 사용자 임의로 히스토리를 작성할 수 있는 페이지의 UI를 구현했습니다.
    - activity_history_edit.xml : 임의 히스토리 작성 페이지와 디자인 통일감을 주기 위해 일부 수정하였습니다.
    - activity_home.xml : Fragment 위치를 일부 조정하였습니다.

--- Application/Project_Footprint/app/src/main/java/com/haerokim/project_footprint/Network/Website.kt
+++ Application/Project_Footprint/app/src/main/java/com/haerokim/project_footprint/Network/Website.kt
@@ -2,6 +2,6 @@ package com.haerokim.project_footprint.Network
 
 class Website {
     companion object{
-        const val BASE_URL = "http://5382fc729837.ngrok.io"
+        const val BASE_URL = "http://e418ab12832e.ngrok.io"
     }
 }
