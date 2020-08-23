2d04b0fe38d6e6a7a32ee23d772a9520dbc57cc2
김율희 <dbfgml741@likelion.org>
2020-08-24 03:52:04 +0900

    [YulHee]

--- Web/Project_Footprint/website/history_serializer.py
+++ Web/Project_Footprint/website/history_serializer.py
@@ -6,10 +6,10 @@ class HistorySerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         data = super().to_representation(instance)
         if not data['created_at']:
-            formatted_date = dateformat.format(timezone.now(), 'Y-m-d H:i:s')
-            data['created_at'] = formatted_date
+            now = timezone.now()
+            data['created_at'] = now
         if not data['mood']:
-            data['mood'] = 0
+            data['mood'] = "기분 좋았던 순간"
         return data
 
     class Meta:
