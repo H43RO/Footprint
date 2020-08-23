f7ee02d13cfa6d22e094526edc4ce4ae1b51906d
김율희 <dbfgml741@likelion.org>
2020-08-24 03:02:44 +0900

    [YulHee] history-api 대응 수정완료했습니다.
    -history_serializer.py : created_at이 비었을 때 대응
    -models.py: 모델 수정
    -history_created:html: css 수정
    -history_update.html: css 수정

--- Web/Project_Footprint/website/history_serializer.py
+++ Web/Project_Footprint/website/history_serializer.py
@@ -1,10 +1,22 @@
 from rest_framework import serializers
 from .models import History
+from django.utils import timezone, dateformat
 
 class HistorySerializer(serializers.ModelSerializer):
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        if not data['created_at']:
+            formatted_date = dateformat.format(timezone.now(), 'Y-m-d H:i:s')
+            data['created_at'] = formatted_date
+        if not data['mood']:
+            data['mood'] = 0
+        return data
+
     class Meta:
         model = History
         fields = '__all__'
+
+
 
 class HistoryPutSerializer(serializers.ModelSerializer):
     class Meta:
