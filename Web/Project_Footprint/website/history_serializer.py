b1b3388de691d8c2b4920d53b4bad8b4ae11652f
김율희 <dbfgml741@likelion.org>
2020-08-24 21:25:47 +0900

    [YulHee] History update API 수정했습니다.
    -history_serializer.py: put 할 때, mood 또는 created_at 필드 값 없을 경우 자동생성

--- Web/Project_Footprint/website/history_serializer.py
+++ Web/Project_Footprint/website/history_serializer.py
@@ -25,5 +25,16 @@ class HistorySerializer(serializers.ModelSerializer):
 class HistoryPutSerializer(serializers.ModelSerializer):
     class Meta:
         model = History
-        fields = ('img','title','mood','comment','place', 'custom_place')
+        fields = ('img','title','mood','comment','place', 'custom_place', 'created_at')
+
+    def to_representation(self, instance):
+        data = super(HistoryPutSerializer, self).to_representation(instance)
+        if not data['created_at']:
+            data['created_at'] = timezone.now()
+            instance.created_at = timezone.now()
+        if not data['mood']:
+            data['mood'] = "기분 좋았던 순간"
+            instance.mood = "기분 좋았던 순간"
+        instance.save()
+        return data
 
