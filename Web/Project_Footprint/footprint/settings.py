caecf3e7a06838f65ed177e422172d4ce3e6cc50
LAPTOP-JHR4BH9T\My Notebook <jinsol0330@naver.com>
2020-08-27 01:06:11 +0900

    [jinsol] : 주석달기, 프론트앤드 구체화 구현

--- Web/Project_Footprint/footprint/settings.py
+++ Web/Project_Footprint/footprint/settings.py
@@ -96,10 +96,10 @@ WSGI_APPLICATION = 'footprint.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME':  os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':  'jinsol',
         'USER': 'root',
-        'PASSWORD': 's9423093',
+        'PASSWORD': '111111',
         'HOST': 'localhost',
         'PORT': '3306'
     }
