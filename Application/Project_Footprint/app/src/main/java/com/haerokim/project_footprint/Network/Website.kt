bba618fd67ea12e7696f3c8e9296ef113aae5b23
H43RO <h2is1234@naver.com>
2020-10-12 01:20:09 +0900

    [HyeonJun] Clean Code
    - ForegroundService.kt : Handler 내의 if 문 예외처리 구문을 간소화하여 가독성을 향상 시킴

--- Application/Project_Footprint/app/src/main/java/com/haerokim/project_footprint/Network/Website.kt
+++ Application/Project_Footprint/app/src/main/java/com/haerokim/project_footprint/Network/Website.kt
@@ -4,6 +4,6 @@ package com.haerokim.project_footprint.Network
 
 class Website {
     companion object{
-        const val BASE_URL = "http://f44d0d4910b0.ngrok.io"
+        const val BASE_URL = "http://96f51c38db5e.ngrok.io"
     }
 }
